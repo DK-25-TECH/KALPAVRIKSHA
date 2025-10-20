6ede16353f60d022104648ab61521a7858114a97
DK-25-TECH <dhayalcric22@gmail.com>
2025-10-20 08:49:33 +0530

    updated code

--- studentprogress.c
+++ studentprogress.c
@@ -1,37 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 struct student
 { 
     int rollNo;
     char name[25];
-    int mark1,mark2,mark3;  
+    int mark1, mark2, mark3;  
 };
+
 char gradeCalc(float avg)
 { 
-    int range = ((int)avg/10);
+    char grade;
+
+    if (avg >= 85)
+        grade = 'A';
+    else if (avg >= 70)
+        grade = 'B';
+    else if (avg >= 50)
+        grade = 'C';
+    else if (avg >= 35)
+        grade = 'D';
+    else
+        grade = 'F';
     
-    switch(range) 
-    { 
-        case 10:
-        case 9:
-        case 8:
-        return 'A';
-        
-        case 7:
-        return 'B';
-        
-        case 6:
-        case 5:
-        return 'C';
-        
-        case 4:
-        return 'D';
-        
-        default:
-        return 'F';
-        
-        } 
-    
+    return grade;
 } 
 
 void printStars(char grade) 
@@ -39,87 +31,86 @@ void printStars(char grade)
     switch(grade)
     { 
         case 'A':
-        printf("PERFORMANCE : *****\n");
-        break; 
+            printf("PERFORMANCE : *****\n");
+            break; 
         
         case 'B':
-        printf("PERFORMANCE : ****\n");
-        break; 
+            printf("PERFORMANCE : ****\n");
+            break; 
         
         case 'C':
-        printf("PERFORMANCE : ***\n"); 
-        break;
+            printf("PERFORMANCE : ***\n"); 
+            break;
         
         case 'D': 
-        printf("PERFORMANCE : **\n");
-        break;
+            printf("PERFORMANCE : **\n");
+            break;
         
         default:
-        printf("PERFORMANCE : FAIL\n");
-        break;
-        
-        }
-        
+            printf("PERFORMANCE : FAIL\n");
+            break;
+    }
 }
-        
-void progressCalculator(struct student s,char (*gradeFptr)(float),void (*starFptr)(char)) 
+
+void progressCalculator(struct student s, char (*gradeFptr)(float), void (*starFptr)(char)) 
 { 
     int total = s.mark1 + s.mark2 + s.mark3;
-    
     float avg = total / 3.0;
-    
     char grade = gradeFptr(avg);
     
-    printf("\nROLL_NO : %d \nNAME : %s\nTOTAL-MARKS : %d \nAVERAGE-MARKS : %.2f\nGRADE : %c\n", s.rollNo,s.name,total,avg,grade);
+    printf("\nROLL_NO : %d \nNAME : %s\nTOTAL-MARKS : %d \nAVERAGE-MARKS : %.2f\nGRADE : %c\n",
+           s.rollNo, s.name, total, avg, grade);
     
     starFptr(grade);
-    
 }
-void displayRollno(struct student s[],int index,int n)
+
+void inputStudents(struct student *s, int n)
 {
-    if(index==n)
-    {
-        return;
+    for (int index = 0; index < n; index++)
+    { 
+        scanf("%d %s %d %d %d",
+              &s[index].rollNo,
+              s[index].name,
+              &s[index].mark1,
+              &s[index].mark2,
+              &s[index].mark3);
     }
-    printf("%d ",s[index].rollNo);
-    displayRollno(s,index+1,n);
+}
+
+void printRollNumbers(struct student s[], int index, int n)
+{
+    if (index == n)
+        return;
+
+    printf("%d ", s[index].rollNo);
+    printRollNumbers(s, index + 1, n);
 }
 
 int main() 
 { 
     int n;
+    scanf("%d", &n);
     
-    scanf("%d",&n);
-    
-    struct student *s = (struct student*) malloc(n * sizeof(struct student));
+    struct student *s = (struct student *) malloc(n * sizeof(struct student));
     if (s == NULL) {
         printf("Memory Allocation Failed!\n");
         return 1;
     }
     
-    for(int index=0; index < n; index++)
-    { 
-        scanf("%d %s %d %d %d",&s[index].rollNo,s[index].name,&s[index].mark1,&s[index].mark2,&s[index].mark3);
-    } 
+    inputStudents(s, n);
     
     printf("\n-- STUDENT ROLL NUMBERS --\n");
-    
-    void(*rollnoFptr)(struct student[],int,int)=displayRollno;
-    
-    rollnoFptr(s,0,n);
+    printRollNumbers(s, 0, n);
+    printf("\n");
     
     char (*gradeFptr)(float) = gradeCalc;
-    
     void (*starFptr)(char) = printStars;
     
-    for(int index = 0; index < n; index++) 
+    for (int index = 0; index < n; index++) 
     {
         progressCalculator(s[index], gradeFptr, starFptr);
-    } 
+    }
     
+    free(s);
+    return 0;
 }
-
-
-
-
-
