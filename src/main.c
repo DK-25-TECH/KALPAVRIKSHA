30bfed415485d7ce2b5a249644ee0b402f103c6e
DK-25-TECH <dhayalcric22@gmail.com>
2025-11-17 21:11:58 +0530

    Delete src directory

--- src/main.c
+++
@@ -1,9 +0,0 @@
-#include "../inc/Playersdata.h"
-#include <stdio.h>
-
-int main() {
-    PlayerNode player;
-    player.playerId = 1;
-    printf("Cricket player system initialized\\n");
-    return 0;
-}
